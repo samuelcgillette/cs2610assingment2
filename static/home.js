012f5d32b3093a755cb1d352c34d54c4fbb294ef
Samuel <samuelcgillette@gmail.com>
2026-02-12 15:29:17 -0700

    added a button that display what is returned by python

--- static/home.js
+++ static/home.js
@@ -1,13 +1,14 @@
 console.log('home.js has been loaded and requested correctly')
+const button = document.getElementById('run-script');
+const randomNumberDiv = document.getElementById('random-number');
 
-async function runPython() {
+button.addEventListener('click', async () => {
     try {
         const response = await fetch('http://localhost:5000/run-script');
         const data = await response.json();
         console.log(data); 
+        randomNumberDiv.textContent = `Random Number from Python: ${data.randomNumber}`;
     } catch (error) {
         console.error("Error calling Python API:", error);
     }
-}
-
-runPython();
+});
